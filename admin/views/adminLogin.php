e40a4d2b89a1917e1891267019694233ee918d12
unknown <astromudanzas@gmail.com>
2023-01-04 07:38:05 +0100

    test

--- admin/views/adminLogin.php
+++ admin/views/adminLogin.php
@@ -3,6 +3,7 @@
 
     <input class="logadmin" type="text" name = "username" placeholder="Usuario">
     
+    
     <input class="logadmin" type="password" name = "password" placeholder="Contraseña">    
     <input id="logadminenviar" type = "submit" value="Iniciar sesión">
 </form>
