65f6a80d8ed3026bde265c2f0253eb949bda6b22
unknown <alexander.baikalov@inslapineda.cat>
2023-01-11 08:50:51 +0000

    title and favicon

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <title>Document</title>
+    <title>Tienda online</title>
 </head>
 <body>
 <?php 
